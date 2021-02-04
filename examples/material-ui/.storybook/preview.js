e875c62f3e18ca90611d2ae826d68e509b9fa3a7
kaznak <kaznak.at.work@gmail.com>
2021-02-04 23:07:48 +0900

    .storybook/preview.js: use material ui theme

--- examples/material-ui/.storybook/preview.js
+++ examples/material-ui/.storybook/preview.js
@@ -1,3 +1,24 @@
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { ThemeProvider } from '@material-ui/core/styles'
+import muiTheme from '../lib/muiTheme'
+
+// https://material-ui.com/components/typography/#install-with-npm
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  layout: 'fullscreen',
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={muiTheme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Story />
+    </ThemeProvider>
+  ),
+]
