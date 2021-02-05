0632ec9f8ebf51087a31990d03cd824875395a89
kaznak <kaznak.at.work@gmail.com>
2021-02-05 23:55:14 +0900

    examples/tailwindcss:  Storybook uses PostCSS 8 with postcss-loader@^4

--- examples/tailwindcss/.storybook/main.js
+++ examples/tailwindcss/.storybook/main.js
@@ -4,4 +4,27 @@ module.exports = {
     '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config) => {
+    /**
+     * CSS handling, specifically overriding postcss loader
+     */
+    // Find the only Storybook webpack rule that tests for css
+    const cssRule = config.module.rules.find((rule) =>
+      'test.css'.match(rule.test)
+    )
+    // Which loader in this rule mentions the custom Storybook postcss-loader?
+    const loaderIndex = cssRule.use.findIndex((loader) => {
+      // Loaders can be strings or objects
+      const loaderString = typeof loader === 'string' ? loader : loader.loader
+      // Find the first mention of "postcss-loader", it may be in a string like:
+      // "@storybook/core/node_modules/postcss-loader"
+      return loaderString.includes('postcss-loader')
+    })
+    // Simple loader string form, removes the obsolete "options" key
+    cssRule.use[loaderIndex] = 'postcss-loader'
+
+    // Uncomment the following to debug
+    // console.dir(config, { depth: null })
+    return config
+  },
 }
