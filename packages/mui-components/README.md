29ce6a9b4ae2f353cad730334056c649f42e69dc
kaznak <kaznak.at.work@gmail.com>
2021-02-07 20:30:40 +0900

    packages/mui-components: add

---
+++ packages/mui-components/README.md
@@ -0,0 +1,19 @@
+# Material UI starter
+
+Features:
+
+- Next.js
+- Typescript
+- Material UI
+- Development Tools
+  - prettier
+  - eslint
+  - jest / react-testing-library
+  - Storybook
+
+## Templates and Packages
+
+- Derived from [Next.js example with typescript eslint jest](https://github.com/vercel/next.js/tree/canary/examples/with-typescript-eslint-jest)
+- [Material UI](https://material-ui.com/)
+  - [and its example for Next.js](https://github.com/mui-org/material-ui/tree/master/examples/nextjs)
+- [Storybook](https://storybook.js.org/)
---
+++ packages/mui-components/pages/index.tsx
@@ -0,0 +1,101 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import {
+  Container,
+  Grid,
+  Box,
+  Paper,
+  Typography,
+  Link,
+  Button,
+} from '@material-ui/core'
+
+export const Home = (): JSX.Element => (
+  <>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Container>
+      <Grid container spacing={10}>
+        <Grid item xs={12}>
+          <Typography variant="h1">
+            Welcome to <a href="https://nextjs.org">Next.js!</a>
+          </Typography>
+          <Typography>
+            Get started by editing <code>pages/index.tsx</code>
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              window.alert('With typescript and Jest')
+            }}
+          >
+            Test Button
+          </Button>
+        </Grid>
+        <Grid item xs={6}>
+          <Link href="https://nextjs.org/docs">
+            <Paper>
+              <Typography variant="h3">Documentation &rarr;</Typography>
+              <Typography>
+                Find in-depth information about Next.js features and API.
+              </Typography>
+            </Paper>
+          </Link>
+        </Grid>
+        <Grid item xs={6}>
+          <Link href="https://nextjs.org/learn">
+            <Paper>
+              <Typography variant="h3">Learn &rarr;</Typography>
+              <Typography>
+                Learn about Next.js in an interactive course with quizzes!
+              </Typography>
+            </Paper>
+          </Link>
+        </Grid>
+        <Grid item xs={6}>
+          <Link href="https://github.com/vercel/next.js/tree/master/examples">
+            <Paper>
+              <Typography variant="h3">Examples &rarr;</Typography>
+              <Typography>
+                Discover and deploy boilerplate example Next.js projects.
+              </Typography>
+            </Paper>
+          </Link>
+        </Grid>
+        <Grid item xs={6}>
+          <Link href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
+            <Paper>
+              <Typography variant="h3">Deploy &rarr;</Typography>
+              <Typography>
+                Instantly deploy your Next.js site to a public URL with Vercel.
+              </Typography>
+            </Paper>
+          </Link>
+        </Grid>
+        <Grid item xs={12} container justify="center" alignContent="center">
+          <Link
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Typography component="div">
+              Powered by{' '}
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                width="128"
+                height="34"
+              />
+            </Typography>
+          </Link>
+        </Grid>
+      </Grid>
+    </Container>
+  </>
+)
+
+export default Home
