944be93d5b8faf90b4ff1694278bd91c6ac26fbf
kaznak <kaznak.at.work@gmail.com>
2021-02-09 23:19:36 +0900

    examples/mui-blog: add NextLinkwrapper

--- examples/mui-blog/components/atomsTmp/NextLinkWrapper.tsx
+++ examples/mui-blog/components/atomsTmp/NextLinkWrapper.tsx
@@ -3,44 +3,46 @@ import React from 'react'
 import MuiLink, { LinkProps as MuiLinkProps } from '@material-ui/core/Link'
 import NextLink, { LinkProps as NextLinkProps } from 'next/link'
 
-export type NextLinkWrapperProps<T> = NextLinkProps &
-  Exclude<T, keyof NextLinkProps>
+export type NextLinkWrapperPropsNarrow<P> = NextLinkProps &
+  Exclude<P, keyof NextLinkProps>
 
-export function NextLinkWrapper<T>(
-  Component // : new () => React.Component<T, any>
-) {
-  return ({
-    // derived from Link props of `next/link`
+export function NextLinkWrapper<P>({
+  component: Component,
+  // derived from Link props of `next/link`
+  href,
+  as,
+  replace,
+  scroll,
+  shallow,
+  passHref = true,
+  prefetch,
+  locale,
+  ...muiLinkProps
+}: NextLinkWrapperPropsNarrow<P> & {
+  component: React.JSXElementConstructor<P>
+}) {
+  const nextLinkProps = {
     href,
     as,
     replace,
     scroll,
     shallow,
-    passHref = true,
+    passHref,
     prefetch,
     locale,
-    ...muiLinkProps
-  }: NextLinkWrapperProps<T>) => {
-    const nextLinkProps = {
-      href,
-      as,
-      replace,
-      scroll,
-      shallow,
-      passHref,
-      prefetch,
-      locale,
-    }
-
-    return (
-      <NextLink {...nextLinkProps}>
-        <Component {...muiLinkProps} />
-      </NextLink>
-    )
   }
+
+  return (
+    <NextLink {...nextLinkProps}>
+      {/* @ts-ignore */}
+      <Component {...muiLinkProps} />
+    </NextLink>
+  )
 }
 
-export type LinkProps = NextLinkWrapperProps<MuiLinkProps>
-export const Link: React.FunctionComponent<LinkProps> = NextLinkWrapper(MuiLink)
+export type LinkProps = NextLinkWrapperPropsNarrow<MuiLinkProps>
+export const Link: React.FunctionComponent<LinkProps> = (props: LinkProps) => (
+  <NextLinkWrapper component={MuiLink} {...props} />
+)
 
 export default Link
